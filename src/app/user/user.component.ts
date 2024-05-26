1bbf90f0d146f022e7f861639b1c02fa9af3a74c
rajgaurav95 <gk.raj.953435@gmail.com>
2024-05-26 22:55:21 +0530

    able to mark the task complete

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,16 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-
-// type User= {
-//   id: string;
-//   avatar: string;
-//   name: string;
-// }
-
-interface User {
-  id: string;
-  avatar: string;
-  name: string;
-}
+import { type User } from './user.model';
 
 @Component({
   selector: 'app-user',
@@ -21,6 +10,7 @@ interface User {
 })
 export class UserComponent {
   @Input({ required: true }) user!: User;
+  @Input({ required: true }) selected!: boolean;
   @Output() select = new EventEmitter();
 
   get imagePath() {
